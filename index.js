a9e65186e1e52afbb0c49e255165673abbe894a3
jubayer-ahmed-sajid <jubayerahmed661ac@gmail.com>
2024-12-21 00:38:01 +0600

    user adding api created

--- index.js
+++ index.js
@@ -2,36 +2,63 @@ const express = require("express");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 
+// middlewares
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vqva6ft.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 const client = new MongoClient(uri, {
-    serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-    }
-  });
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
-  async function run() {
-    try {
-      // Connect the client to the server	(optional starting in v4.7)
-      await client.connect();
-      // Send a ping to confirm a successful connection
-      await client.db("admin").command({ ping: 1 });
-      console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-      // Ensures that the client will close when you finish/error
+const userCollection = client.db("my-book-shop").collection("Users");
+const booksCollection = client.db("my-book-shop").collection("books");
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+
+    // insert user
+    app.post("/users", async (req, res) => {
+      const userInfo = req.body;
+      const query = {email: req.body.email}
+      const user = userCollection.findOne(query)
+      if(user){
+        return res.send({message: "User already exist"})
+      }
+      console.log(userInfo);
+      const result = await userCollection.insertOne(userInfo);
+      res.send(result);
+    });
+  } finally {
+    // Ensures that the client will close when you finish/error
     //   await client.close();
-    }
   }
-  run().catch(console.dir);
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
-    res.send("Server is running");
-  });
-  app.listen(port, () => {
-    console.log(`Server is running at port ${port}`);
-  })
+  res.send("Server is running");
+});
+app.listen(port, () => {
+  console.log(`Server is running at port ${port}`);
+});
